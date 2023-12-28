9bda60488c975596b33edf5b75fbb924794ce754
Vinícius Vilar <viniciusvillarma7x@gmail.com>
2023-12-28 08:39:30 -0300

    feat: Criando as entidades Produto e Categoria e as migrations

---
+++ ApiCatalogo/ApiCatalogo/Models/Categoria.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.ObjectModel;
+
+namespace ApiCatalogo.Models;
+
+public class Categoria
+{
+    public Categoria()
+    {
+        Produtos = new Collection<Produto>();
+    }
+    public int CategoriaId { get; set; }
+    public string Nome { get; set; }
+    public string ImageUrl { get; set; }
+
+    public ICollection<Produto>? Produtos { get; set; }
+}
