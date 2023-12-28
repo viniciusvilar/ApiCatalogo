bf60ab3f55caf92fc778c34448732c5adf075fbe
Vinícius Vilar <viniciusvillarma7x@gmail.com>
2023-12-28 09:45:37 -0300

    refactor: Alterando tamanho das propriedades de Produto e Categoria

--- ApiCatalogo/ApiCatalogo/Models/Produto.cs
+++ ApiCatalogo/ApiCatalogo/Models/Produto.cs
@@ -1,11 +1,23 @@
-﻿namespace ApiCatalogo.Models;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
+namespace ApiCatalogo.Models;
+
+[Table("Produtos")]
 public class Produto
 {
+    [Key]
     public int ProdutoId { get; set; }
+    [Required]
+    [StringLength(80)]
     public string Nome { get; set; }
+    [Required]
+    [StringLength(300)]
     public string Descricao { get; set; }
+    [Column(TypeName ="decimal(10,2)")]
     public decimal Preco { get; set; }
+    [Required]
+    [StringLength(300)]
     public string ImageUrl { get; set; }
     public float Estoque { get; set; }
     public DateTime DataCadastro { get; set; }
